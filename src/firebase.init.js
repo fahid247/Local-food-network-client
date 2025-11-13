9afac1bf2496230701a8ea4ce4ea386e57102757
Fahid Aqundow <136666497+fahid247@users.noreply.github.com>
2025-11-13 23:32:31 +0600

    done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKA1SCABIaJ1XOIWelXua34P36b_8lowA",
-  authDomain: "local-foods-network.firebaseapp.com",
-  projectId: "local-foods-network",
-  storageBucket: "local-foods-network.firebasestorage.app",
-  messagingSenderId: "334150845021",
-  appId: "1:334150845021:web:eefc1d5b02d369324c03b4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
